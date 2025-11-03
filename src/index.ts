e359e33d2eb44441df760bc3220f7a93f169c175
shubham73855 <shubhamtiwari9405@gmail.com>
2025-11-03 12:12:25 +0530

    port assigen by .env file

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
 
 // Routes
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript + Express!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, TypeScript + Express!");
 });
 
 // Start server
