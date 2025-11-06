8bc2f354ccdd6f59257a46662c67c328e496556e
shubham73855 <shubhamtiwari9405@gmail.com>
2025-11-06 12:27:30 +0530

    user model is updated

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,16 +1,40 @@
 import mongoose, { Schema, Document } from "mongoose";
+import bcrypt from "bcrypt";
 
 export interface IUser extends Document {
   name: string;
   email: string;
+  password: string;
+  comparePassword(candidate: string): Promise<boolean>;
 }
 
-const userSchema: Schema = new Schema(
+const userSchema = new Schema<IUser>(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
   },
   { timestamps: true }
 );
+
+// Hash password before saving
+userSchema.pre<IUser>("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password as string, salt);
+    next();
+  } catch (err) {
+    next(err as Error);
+  }
+});
+
+// Compare passwords
+userSchema.methods.comparePassword = async function (
+  candidate: string
+): Promise<boolean> {
+  return bcrypt.compare(candidate, this.password as string);
+};
 
 export default mongoose.model<IUser>("User", userSchema);
